3a541b56fa47302bd188bec83da781c5ab98ea35
Sebastian Lay <mail@sebastian-lay.de>
2022-10-07 02:58:29 +0200

    Just the unusual suspects

---
+++ Unusual Database Program/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const int DefaultPort = 19114;
+
+int port = args.Length > 0 && int.TryParse(args[0], out port) ? port : DefaultPort;
+var listener = new UdpClient(port);
+
+var database = new Dictionary<string, string> {
+    { "version", "My Unusual Database Program 1.0" }
+};
+
+Console.WriteLine($"Listening on port {port}");
+
+try
+{
+    while (true)
+    {
+        var result = await listener.ReceiveAsync();
+        HandleMessageAsync(result.Buffer, result.RemoteEndPoint);
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+finally
+{
+    listener.Close();
+}
+
+async Task HandleMessageAsync(byte[] buffer, IPEndPoint endpoint)
+{
+    try
+    {
+        var message = Encoding.ASCII.GetString(buffer);
+        Console.WriteLine($"< {message}");
+        if (message.Contains('='))
+        {
+            // insert request
+            var index = message.IndexOf('=');
+            var key = message[..index];
+            if (key == "version")
+                return;
+            var value = message[(index + 1)..];
+            database[key] = value;
+        }
+        else
+        {
+            // retrieve request
+            var response = database.TryGetValue(message, out var value)
+                ? $"{message}={value}"
+                : $"{message}=";
+            Console.WriteLine($"> {response}");
+            var result = Encoding.ASCII.GetBytes(response);
+            await listener.SendAsync(result, endpoint);
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+}
